7f0e1befe6c5aa1dc5686f9ad2f872909044e44a
Ruslana Babich <r.babich@g.nsu.ru>
2025-09-06 23:15:57 +0700

    add main test

--- Task_1_1_1/src/test/java/ru/nsu/babich/MainTest.java
+++ Task_1_1_1/src/test/java/ru/nsu/babich/MainTest.java
@@ -47,4 +47,8 @@ class MainTest {
         assertArrayEquals(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}, array);
     }
 
+    @Test
+    void checkMain() {
+        Main.main(new String[]{});
+    }
 }
