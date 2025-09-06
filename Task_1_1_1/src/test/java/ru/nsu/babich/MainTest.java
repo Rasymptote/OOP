42d26f91848b2f932cd88a8d3ed9eafb02ee0f11
Ruslana Babich <r.babich@g.nsu.ru>
2025-09-06 23:11:01 +0700

    add more heapsort tests

--- Task_1_1_1/src/test/java/ru/nsu/babich/MainTest.java
+++ Task_1_1_1/src/test/java/ru/nsu/babich/MainTest.java
@@ -20,7 +20,7 @@ class MainTest {
     }
 
     @Test
-    void checkDublicates() {
+    void checkDuplicates() {
         var array = new int[]{8, 8, 8, 3, 67, 2, 2, 1, 7};
         Main.heapsort(array);
         assertArrayEquals(new int[]{1, 2, 2, 3, 7, 8, 8, 8, 67}, array);
@@ -31,6 +31,20 @@ class MainTest {
         var array = new int[]{0};
         Main.heapsort(array);
         assertArrayEquals(new int[]{0}, array);
+    }
+
+    @Test
+    void checkAlreadySorted() {
+        var array = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+        Main.heapsort(array);
+        assertArrayEquals(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}, array);
+    }
+
+    @Test
+    void checkReverseSorted() {
+        var array = new int[]{11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+        Main.heapsort(array);
+        assertArrayEquals(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}, array);
     }
 
 }
