6748c3c9c35f6a8c381c4910e7d89cbb1a0deb44
Ruslana Babich <r.babich@g.nsu.ru>
2025-10-27 10:47:11 +0700

    add toString tests

--- Task_1_2_1/src/test/java/ru/nsu/babich/AdjacencyMatrixTest.java
+++ Task_1_2_1/src/test/java/ru/nsu/babich/AdjacencyMatrixTest.java
@@ -1,9 +1,33 @@
 package ru.nsu.babich;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
 class AdjacencyMatrixTest extends GraphTest {
 
     @Override
     protected Graph createGraph() {
         return new AdjacencyMatrix();
+    }
+
+    @Test
+    void checkToString() {
+        Graph adjMatrix = createGraph();
+        var a = new Vertex("a");
+        var b = new Vertex("b");
+        var c = new Vertex("c");
+
+        adjMatrix.addEdge(new Edge(a, c));
+        adjMatrix.addEdge(new Edge(c, b));
+        adjMatrix.addEdge(new Edge(b, b));
+
+        assertEquals("""
+                        0 1 0\s
+                        0 0 1\s
+                        0 0 1\s
+                        """,
+                adjMatrix.toString()
+        );
     }
 }
