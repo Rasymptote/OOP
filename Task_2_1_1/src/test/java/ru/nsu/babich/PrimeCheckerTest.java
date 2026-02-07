b04bfc2e32f66dd0065f7c4c2b93b8ae78a5564b
Ruslana Babich <r.babich@g.nsu.ru>
2026-02-07 23:03:57 +0700

    fix indentations

--- Task_2_1_1/src/test/java/ru/nsu/babich/PrimeCheckerTest.java
+++ Task_2_1_1/src/test/java/ru/nsu/babich/PrimeCheckerTest.java
@@ -24,16 +24,16 @@ public abstract class PrimeCheckerTest {
                 Arguments.of(new int[]{2, 3, 5, 7, 11}, false),
                 Arguments.of(new int[]{4, 6, 8, 9, 10}, true),
                 Arguments.of(new int[]{13, 17, 19, 23}, false),
-                Arguments.of( new int[]{12, 15, 18, 20}, true),
+                Arguments.of(new int[]{12, 15, 18, 20}, true),
                 Arguments.of(new int[]{29, 31, 37, 41}, false),
                 Arguments.of(new int[]{14, 21, 25, 28}, true),
                 Arguments.of(new int[]{2}, false),
                 Arguments.of(new int[]{1}, true),
                 Arguments.of(new int[]{0}, true),
                 Arguments.of(new int[]{1000000007, 1000000009, 1000000033,
-                        1000000087, 1000000093, 1000000097}, false),
+                    1000000087, 1000000093, 1000000097}, false),
                 Arguments.of(new int[]{1000000123, 1000000181, 1000000207,
-                        1000000223, 1000000241, 1000000272, 1000000289}, true)
+                    1000000223, 1000000241, 1000000272, 1000000289}, true)
         );
     }
 }
